4b82c28996450bc16f4b024659e4baa98e64f395
amealf <amealf@163.com>
2020-07-04 01:32:43 +0800

    Update plt.figure&axes example.py

--- plt.figure&axes example.py
+++ plt.figure&axes example.py
@@ -11,19 +11,20 @@ t = np.arange(0.0, 2.0, 0.01)
 fig = plt.figure(figsize=(19, 9.8))
 
 #set the size of subplots
-left = 0.14
-width = 0.77
-bottom = 0.11 
-height = 0.5
-bottom_h= bottom + height + 0.04
+left = 0.03
+width = 0.95
+bottom = 0.04
+height = 0.25
+bottom_h = bottom + height + 0.04 #  0.11+0.5+0.04=0.65
+print(bottom_h)
 
-rect_line1=[left,bottom,width,height]
-rect_line2=[left,bottom_h,width,0.3]
+rect_line1 = [left,bottom,width,0.25] #  below parameter
+rect_line2 = [left,bottom_h,width,0.65]#  upper parameter
 
-axbelow=plt.axes(rect_line1)
-axupper=plt.axes(rect_line2)
+axbelow = plt.axes(rect_line1) #  below ax
+axupper = plt.axes(rect_line2) #  upper ax
 
-plot1=axbelow.plot(t,np.sin(2*np.pi*t),'-ob',ms=1)
-plot2=axupper.plot(t,np.sin(2*np.pi*t),'-og',ms=1)
+plot1 = axbelow.plot(t,np.sin(2*np.pi*t),'-ob',ms=1)
+plot2 = axupper.plot(t,np.sin(2*np.pi*t),'-og',ms=1)
 
 plt.show()
